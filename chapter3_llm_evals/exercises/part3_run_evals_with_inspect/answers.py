d6dd704fefb3d0d7749179c581fc6ae082debdff
tinuademargaret <summittinuade@gmail.com>
2024-09-18 11:18:47 +0000

    p2

--- chapter3_llm_evals/exercises/part3_run_evals_with_inspect/answers.py
+++ chapter3_llm_evals/exercises/part3_run_evals_with_inspect/answers.py
@@ -123,3 +123,146 @@ def prompt_template(template: str) -> Solver:
 
 tests.test_solver_functions(solver_functions=prompt_template(template=""))
 # %%
+
+@solver
+def multiple_choice_format(template: str, **params: dict) -> Solver:
+    """
+    Returns a solve function which modifies the initial prompt to be in the format of a multiple choice question. Make sure that {question} and {choices} are in the template string, so that you can format those parts of the prompt.
+
+    Args:
+        template : The template string to use to modify the user prompt. Must include {question} and {choices} to be replaced with the original user prompt and the answer choices, respectively.
+
+    Returns:
+        solve : A solve function which modifies the user prompt with the given template
+    """
+
+
+    async def solve(state: TaskState, generate: Generate) -> TaskState:
+        if state.user_prompt and state.choices:
+            state.user_prompt.text = template.format(
+                question=state.user_prompt.text,
+                choices=answer_options(state.choices),
+                **params,
+            )
+        return state
+
+
+    return solve
+
+# tests.test_solver_functions(solver_functions=multiple_choice_format(
+#     template="hello, i'm a {question} with {choices}")
+# )
+
+
+@solver
+def system_message(system_message: str, **params: dict) -> Solver:
+    """
+    Returns a solve function which inserts a system message with the given content into the state.messages list. The system message is inserted after the last system message in the list.
+
+    Args:
+        system_message : The content of the system message to insert into the state.messages list
+
+    Returns:
+        solve : A solve function which inserts a system message with the given content into the state.messages list
+    """
+    def insert_system_message(messages: list[ChatMessageUser | ChatMessageSystem | ChatMessageAssistant], message: ChatMessageSystem):
+        """
+        Inserts the given system message into the messages list after the last system message in the list.
+
+        Args:
+            messages : The list of messages to insert the system message into
+            message : The system message to insert into the list
+
+        Returns:
+            None
+        """
+        lastSystemMessageIndex = -1
+        for i in list(reversed(range(0, len(messages)))):
+            if isinstance(messages[i], ChatMessageSystem):
+                lastSystemMessageIndex = i
+                break
+        messages.insert(lastSystemMessageIndex + 1, message)
+
+    async def solve(state: TaskState, generate: Generate) -> TaskState:
+        return state
+
+    return solve
+
+@solver
+def self_critique(
+    model: str,
+    critique_template: str | None,
+    critique_completion_template: str | None,
+    **params: dict,
+) -> Solver:
+    """
+    Args:
+        - model: The model we use to generate the self-critique
+
+        - critique_template: This is the template for how you present the output of the model so far to itself, so that the model can *generate* the critique. This template should contain {question} and {completion} to be formatted.
+
+        - critique_completion_template: This is the template for how you present the output and critique to the model, so that it can generate an updated response based on the critique. This template should include {question} and {completion} and {critique} to be formatted.
+
+    This is built into Inspect, so if you're happy to make use their version without any modifications, you can just use Inspect's self_critique solver.
+    """
+
+
+    async def solve(state: TaskState, generate: Generate) -> TaskState:
+        metadata = omit(state.metadata, ["question", "completion", "critique"])
+        Model = get_model(model)
+        critique = await Model.generate(
+            critique_template.format(
+                question=state.input_text,
+                completion=state.output.completion,
+                **metadata,
+            )
+        )
+
+        state.messages.append(
+            ChatMessageUser(
+                content=critique_completion_template.format(
+                    question=state.input_text,
+                    completion=state.output.completion,
+                    critique=critique.completion,
+                    **metadata,
+                )
+            )
+        )
+
+        return await generate(state)
+
+    return solve
+
+self_critique_critique_template = r"""
+Given the following question and answer, please critique the answer. A good answer comprehensively answers the question and NEVER refuses to answer. If the answer is already correct do not provide critique - simply respond 'The original answer is fully correct'.
+
+[BEGIN DATA]
+***
+[Question]: {question}
+***
+[Answer]: {completion}
+***
+[END DATA]
+
+Critique: """
+
+self_critique_completion_template = r"""
+Given the following question, initial answer and critique please generate an improved answer to the question:
+
+[BEGIN DATA]
+***
+[Question]: {question}
+***
+[Answer]: {completion}
+***
+[Critique]: {critique}
+***
+[END DATA]
+
+If the original answer is already correct, just repeat the original answer exactly. Provide your answer at the end on its own line in the form "ANSWER: $ANSWER" (without quotes) where $ANSWER is the answer to the question.
+"""
+
+chain_of_thought_template = r"""{prompt}\n\nBefore answering, think through how you want to answer the question. Make sure to think step-by-step and weigh all relevant concerns, and then arrive at a conclusive answer."""
+multiple_choice_format_template = r"""{question}\n\n{choices}"""
+make_choice_prompt = r"""Now provide your conclusion in the form ANSWER: $LETTER where $LETTER is one of A, B. Only output in this format."""
+system_message_prompt = r"""You are a harmless, helpful, and honest AI chat assistant."""
