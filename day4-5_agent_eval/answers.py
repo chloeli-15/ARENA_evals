4479325d3a0ca137dae640dca7876fa77322daa5
tinuademargaret <summittinuade@gmail.com>
2024-09-19 10:49:25 +0000

    implements ArithmeticTask

--- day4-5_agent_eval/answers.py
+++ day4-5_agent_eval/answers.py
@@ -18,8 +18,6 @@ import math
 import re
 from pathlib import Path
 import sys
-sys.path.insert(0, "/root/ARENA_evals/chapter3_llm_evals/exercises/part4_llm_agents/")
-import solutions
 import part4_llm_agents.solutions as solutions
 
 #Make sure exercises are in the path
@@ -40,4 +38,96 @@ openai.api_key = api_key
 
 client = OpenAI()
 # %%
-``
+import math
+
+class ArithmeticTask:
+    def __init__(self, num1: int | float, num2: int | float):
+        self.num1 = num1
+        self.num2 = num2
+        self.operations: List[str] = ["+", "-", "*", "/", "%", "//"]
+        self.correct_answers: Dict[str, float] = self._generate_answers()
+        self.is_solved: Dict[str, bool] = {expr: False for expr in self.correct_answers}
+        self.current_task_number = 0
+
+    def _generate_answers(self) -> Dict[str, float]:
+        """
+        Generates a dictionary the correct answers for all possible tasks
+
+        Returns:
+            Dict[str, float]: A dictionary with the expression as key and the correct answer as value
+        """
+        correct_answers = {}
+        for operation in self.operations:
+            if operation == "+":
+                answer = self.num1 + self.num2
+            if operation == "-":
+                answer = self.num1 - self.num2
+            if operation == "*":
+                answer = self.num1 * self.num2
+            if operation == "/":
+                answer = self.num1 / self.num2
+            if operation == "%":
+                answer = self.num1 % self.num2
+            if operation == "//":
+                answer = self.num1 // self.num2
+
+            correct_answers[str(self.num1) + operation + str(self.num2)] = answer
+
+        return correct_answers
+
+    @property
+    def get_current_task(self) -> str:
+        """
+        Gets the current task for the agent
+
+        Returns:
+            str: A string containing the current task
+        """
+        return str(self.current_task_number)
+
+    @property
+    def current_task_instruction(self) -> str:
+        """
+        Gets a string containing instructions for the current task for the agent. This will be fed to the agent as a user prompt.
+
+        Returns:
+            str: A string containing the instructions for the current task
+        """
+        return list(self.correct_answers.keys())[self.current_task_number]
+
+    def check_solved(self) -> bool:
+        """
+        Checks if all tasks have been solved
+
+        Returns:
+            bool: True if all tasks have been solved, False otherwise
+        """
+        return all(list(self.is_solved.values()))
+
+    def check_answer(self, model_answer: str) -> bool:
+        """
+        Checks if the model's answer is correct
+
+        Args:
+            model_answer (str): The model's answer
+
+        Returns:
+            bool: True if the model's answer is correct, False otherwise
+        """
+        answer = list(self.correct_answers.keys())[self.get_current_task()]
+        return math.isclose(answer, float(model_answer))
+
+    def update_current_task(self):
+        """
+        Sets is_solved for the current task to True and increments self.current_task_number by one
+        """
+        current_task = list(self.correct_answers.keys())[self.get_current_task()]
+        self.is_solved[current_task] = True
+        self.current_task_number += 1        
+
+tests.ArithmeticTaskTests(ArithmeticTask)
+
+x = ArithmeticTask(10, 15)
+for problem, answer in x.correct_answers.items():
+    print(f"{problem} = {answer}")
+# %%
