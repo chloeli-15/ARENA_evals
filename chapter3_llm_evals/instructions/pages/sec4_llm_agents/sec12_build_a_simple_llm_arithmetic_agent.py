65fcebb1c7ef39e577dd21258de5b2dfc780dea6
James Hindmarch <styme3279@gmail.com>
2024-09-19 11:33:52 +0100

    changed type function

--- chapter3_llm_evals/instructions/pages/sec4_llm_agents/sec12_build_a_simple_llm_arithmetic_agent.py
+++ chapter3_llm_evals/instructions/pages/sec4_llm_agents/sec12_build_a_simple_llm_arithmetic_agent.py
@@ -334,19 +334,21 @@ Models like ChatGPT and Claude are fine-tuned to interpret and respond to `tool`
 ```python
 {
     "type": "function",
-    "name": "get_delivery_date",
-    "description": "Get the delivery date for a customer's order. Call this whenever you need to know the delivery date, for example when a customer asks 'Where is my package'",
-    "parameters": {
-        "type": "object",
-        "properties": {
-            "order_id": {
-                "type": "string",
-                "description": "The customer's order ID.",
+    "function": {
+        "name": "get_delivery_date",
+        "description": "Get the delivery date for a customer's order. Call this whenever you need to know the delivery date, for example when a customer asks 'Where is my package'",
+        "parameters": {
+            "type": "object",
+            "properties": {
+                "order_id": {
+                    "type": "string",
+                    "description": "The customer's order ID.",
+                },
             },
+            "required": ["order_id"],
+            "additionalProperties": false,
         },
-        "required": ["order_id"],
-        "additionalProperties": false,
-    }
+    },
 }
 ```
 
