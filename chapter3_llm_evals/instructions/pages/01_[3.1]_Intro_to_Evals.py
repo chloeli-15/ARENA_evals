508ef968afce7d73b1824471e6a3b04156cef0c6
James Hindmarch <styme3279@gmail.com>
2024-11-29 11:19:52 +0000

    day 1 master files

--- chapter3_llm_evals/instructions/pages/01_[3.1]_Intro_to_Evals.py
+++ chapter3_llm_evals/instructions/pages/01_[3.1]_Intro_to_Evals.py
@@ -1,43 +1,58 @@
-import os, sys
+import platform
+import sys
 from pathlib import Path
 
-instructions_dir = Path(__file__).parent.parent.resolve()
-if str(instructions_dir) not in sys.path: sys.path.append(str(instructions_dir))
-os.chdir(instructions_dir)
-
 import streamlit as st
-
-import st_dependencies
-st_dependencies.styling()
-
-from pages.sec1_intro_to_evals import (
-    sec10_home,
-    sec11_threat_model,
-    sec12_intro_to_api, 
-    sec13_mcq_benchmark,
-)
-
-import platform
-is_local = (platform.processor() != "")
-
-
 import streamlit_antd_components as sac
 
+# metadata should be a list of dicts like:
+# [
+#     {"title": "TMS", "icon": "1-circle-fill", "subtitle": "(99%)"},
+#     {"title": "Bonus", "icon": "star", "subtitle": "(1%)"},
+#     ...
+# ]
+metadata = [
+    {"title": "Threat-Modeling", "icon": "1-circle-fill", "subtitle": "45%"},
+    {"title": "Intro to API Calls", "icon": "2-circle-fill", "subtitle": "5%"},
+    {"title": "MCQ Benchmarks", "icon": "3-circle-fill", "subtitle": "50%"},
+]
+chapter_name = "chapter3_llm_evals"
+
+pages_dir = Path(__file__).parent  # ARENA_3/chapter_name/instructions/pages
+instructions_dir = pages_dir.parent  # ARENA_3/chapter_name/instructions
+chapter_dir = instructions_dir.parent  # ARENA_3/chapter_name
+arena_root_dir = chapter_dir.parent  # ARENA_3
+if str(arena_root_dir) not in sys.path:
+    sys.path.append(str(arena_root_dir))
+
+from st_dependencies import generate_toc, styling
+
+markdown_content_file = Path(__file__).with_suffix(".md")
+
+# TODO - uncomment the line below, so page is cached (while developing it's easier to comment this out)
+# if st.session_state.get("content_filename", None) != str(markdown_content_file):
+st.session_state["content_filename"] = str(markdown_content_file)
+st.session_state["content"] = markdown_content_file.read_text(encoding="utf-8").split(
+    "=== NEW CHAPTER ==="
+)
+
+IS_LOCAL = platform.processor() != ""
+DEBUG = False
+
+styling(DEBUG)
+
 with st.sidebar:
-    st.markdown('')
-    
-    CHAPTER = sac.steps([
-        sac.StepsItem(title='Home', icon="house"),
-        sac.StepsItem(title='Threat-Modeling', subtitle='(45%)', icon="1-circle-fill"),
-        sac.StepsItem(title='Intro to API Calls', subtitle='(5%)', icon="2-circle-fill"),
-        sac.StepsItem(title='MCQ Benchmarks', subtitle='(50%)', icon="3-circle-fill")
-    ], size='small', return_index=True)
+    CHAPTER_SELECT = sac.steps(
+        [
+            sac.StepsItem(title="Home", icon="house"),
+            *[sac.StepsItem(**data) for data in metadata],
+        ],
+        size="small",
+        return_index=True,
+    )
+    chapter_content = st.session_state["content"][int(CHAPTER_SELECT)]
+    table_of_contents = generate_toc(chapter_content)
 
-    function = [
-        sec10_home.section,
-        sec11_threat_model.section,
-        sec12_intro_to_api.section,
-        sec13_mcq_benchmark.section
-    ][CHAPTER]
+    st.markdown(table_of_contents, unsafe_allow_html=True)
 
-function()
+st.markdown(chapter_content, unsafe_allow_html=True)
