1558b23e44984d7add8bb901fddeacc70b56b994
James Hindmarch <styme3279@gmail.com>
2024-12-09 17:05:07 +0000

    fixed a test function

--- chapter3_llm_evals/exercises/part3_evals_with_inspect/tests.py
+++ chapter3_llm_evals/exercises/part3_evals_with_inspect/tests.py
@@ -104,6 +104,7 @@ def test_solver_functions(solver_functions, test_dataset : Optional[list[Sample]
         model = "openai/gpt-4o-mini",
         log_dir = "./logs_from_solver_tests/"
     )
-    return f"Now call \"!inspect view\" with on the following log to see how your solvers worked  /logs_from_solver_tests/{log.eval.created}_{log.eval.task}_{log.eval.task_id}.json"
+    try: f"Now call \"!inspect view\" with on the following log to see how your solvers worked  /logs_from_solver_tests/{log[0].eval.created}_{log[0].eval.task}_{log[0].eval.task_id}.json"
+    except: f"Now call \"!inspect view\" with on the following log to see how your solvers worked  /logs_from_solver_tests/{log.eval.created}_{log.eval.task}_{log.eval.task_id}.json"
 
 # %%
